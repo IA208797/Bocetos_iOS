5500f4071f1631b613dacefc3435c8533ad21f5d
Izant <83232702+IA208797@users.noreply.github.com>
2024-10-11 10:42:48 -0600

    [AGREGA] Un chingo de cosas también

---
+++ boceto_2_CellView/boceto_2_CellView/controladores_de_pantallas/estilos/EstiloCellViewControllerCelular.swift
@@ -0,0 +1,8 @@
+//
+//  EstiloCellViewControllerCelular.swift
+//  boceto_2_CellView
+//
+//  Created by alumno on 11/10/24.
+//
+
+import Foundation
---
+++ boceto_2_CellView/boceto_2_CellView/controladores_de_pantallas/vista_de_celdas/VistaDeZelda.swift
@@ -0,0 +1,14 @@
+//
+//  VistaDeZelda.swift
+//  boceto_2_CellView
+//
+//  Created by alumno on 11/10/24.
+//
+
+import UIKit
+
+class VistaDeZelda: UICollectionViewCell{
+    @IBOutlet weak var etiqueta: UILabel!
+    
+    @IBOutlet weak var cuerpo: UILabel!
+}
